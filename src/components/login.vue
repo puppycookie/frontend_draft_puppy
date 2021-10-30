3de24a09ff5611312345d64ce0a6213d853e6a18
pookie <864770586@qq.com>
2021-10-30 18:39:53 +0800

    加了输入框

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,9 +1,22 @@
 <template>
     <div class="login_container">
         <div class="login_box">
-            <div class="avatar_box">
+            <div class="avatar_box"><!-- 头像 -->
                 <img src="../assets/xiaogougou.png" alt="">
             </div>
+            <!-- 登录表单  -->
+            <el-form label-width="40px" class="login_form">
+                <el-form-item label="邮箱">
+                    <el-input></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input></el-input>
+                </el-form-item>
+                <el-form-item class="btns">
+                    <el-button type="primary">登录</el-button>
+                    <el-button type="primary">验证码登录</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
@@ -44,4 +57,17 @@
             border-radius: 50%;
         }
     }
+
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0 20px;
+}
 </style>
